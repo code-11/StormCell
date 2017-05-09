e8166b32a5002d2d251569f81b27ed6fad25d95b
code-11 <brendan.ritter@yahoo.com>
2017-05-08 23:58:14 -0400

    Added selection of units

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	public Sprite haloSprite;
+	GameObject selectionHaloObj;
+
+	public void onSelected(){
+		//Only allow to be selected once
+		if (transform.childCount == 0) {
+			selectionHaloObj = new GameObject (); 
+			//Center on parent
+			selectionHaloObj.name = "Selection Halo";
+			selectionHaloObj.transform.SetParent (gameObject.transform);
+			selectionHaloObj.transform.localPosition = new Vector3 (0, 0, 0);
+			selectionHaloObj.transform.localScale = new Vector3 (1, 1, 1);
+			selectionHaloObj.AddComponent<SpriteRenderer> ();
+			SpriteRenderer haloRenderer = selectionHaloObj.GetComponent<SpriteRenderer> ();
+			haloRenderer.sprite = haloSprite;
+			haloRenderer.color = Color.green;
+			haloRenderer.sortingOrder=1;
+		} else {
+			Debug.Log (gameObject.name + " already has a halo");
+			Debug.Log (gameObject.transform.GetChild (0).name);
+		}
+	
+	}
+}
