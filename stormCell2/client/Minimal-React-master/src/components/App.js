e5ac0eed7e41f77c37283d5fb39349c3fc0431e3
code-11 <brendan.ritter@yahoo.com>
2019-10-25 17:57:23 -0400

    Using correct OLMap library now

--- stormCell2/client/Minimal-React-master/src/components/App.js
+++ stormCell2/client/Minimal-React-master/src/components/App.js
@@ -1,23 +1,64 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import OlMap from "ol/map";
+import OlView from "ol/View";
+import OlLayerTile from "ol/layer/tile";
+import OlSourceOSM from "ol/source/osm";
 
-import {
-  interaction, layer, custom, control, //name spaces
-  Interactions, Overlays, Controls,     //group
-  Map, Layers, Overlay, Util    //objects
-} from "react-openlayers";
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => (
-    <Map view={{center: [0, 0], zoom: 2}}>
-      <Layers>
-        <layer.Tile/>
-      </Layers>
-      <Controls attribution={false} zoom={false}>
-        <control.Rotate />
-      </Controls>
-      <Interactions>
-      </Interactions>
-    </Map>
-);
+    this.state = { center: [0, 0], zoom: 1 };
+
+    this.olmap = new OlMap({
+      target: null,
+      layers: [
+        new OlLayerTile({
+          source: new OlSourceOSM()
+        })
+      ],
+      controls: [],
+      view: new OlView({
+        center: this.state.center,
+        zoom: this.state.zoom
+      })
+    });
+  }
+
+  updateMap() {
+    this.olmap.getView().setCenter(this.state.center);
+    this.olmap.getView().setZoom(this.state.zoom);
+  }
+
+  componentDidMount() {
+    this.olmap.setTarget("map");
+
+    // Listen to map changes
+    this.olmap.on("moveend", () => {
+      let center = this.olmap.getView().getCenter();
+      let zoom = this.olmap.getView().getZoom();
+      this.setState({ center, zoom });
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    let center = this.olmap.getView().getCenter();
+    let zoom = this.olmap.getView().getZoom();
+    if (center === nextState.center && zoom === nextState.zoom) return false;
+    return true;
+  }
+
+  userAction() {
+    this.setState({ center: [546000, 6868000], zoom: 5 });
+  }
+
+  render() {
+    this.updateMap(); // Update map on render?
+    return (
+      <div id="map" style={{ width: "100%", height: "100vh" }}>
+      </div>
+    );
+  }
+}
 
 export default App;
