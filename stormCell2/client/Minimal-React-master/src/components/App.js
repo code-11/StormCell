e79f1df1afcdc58910385fa3d4519d2f7d7345b8
code-11 <brendan.ritter@yahoo.com>
2019-10-26 15:22:14 -0400

    Got a minimal country map up

--- stormCell2/client/Minimal-React-master/src/components/App.js
+++ stormCell2/client/Minimal-React-master/src/components/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
-import OlMap from "ol/map";
-import OlView from "ol/View";
-import OlLayerTile from "ol/layer/tile";
-import OlSourceOSM from "ol/source/osm";
+import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import Style from 'ol/style/Style';
+import Stroke from 'ol/style/Stroke';
+import Fill from 'ol/style/Fill';
+import countryData from "./countries";
+
+//4326 - LAT LON
+//3857 - X, Y
 
 class App extends Component {
   constructor(props) {
@@ -10,19 +21,50 @@ class App extends Component {
 
     this.state = { center: [0, 0], zoom: 1 };
 
-    this.olmap = new OlMap({
-      target: null,
-      layers: [
-        new OlLayerTile({
-          source: new OlSourceOSM()
-        })
+    const countryLoader = new VectorSource({
+      format: new GeoJSON({dataProjection: 'EPSG:4326'})
+    })
+    countryLoader.addFeatures((new GeoJSON()).readFeatures(countryData));
+
+    const style = new Style({
+          stroke: new Stroke({
+              color: 'black',
+              width: 1
+          }),
+          // text: new Text({
+          //   font: '12px Calibri,sans-serif',
+          //   fill: new Fill({
+          //     color: '#000'
+          //   }),
+          //   stroke: new Stroke({
+          //     color: '#fff',
+          //     width: 3
+          //   })
+          // })
+      });
+
+    const countryLayer= new VectorLayer({
+      source: countryLoader,
+      style: function(feature, res){
+      // replace "Germany" with any country name you would like to display...
+          // const text=style.getText();
+          // text.setText(feature.get('name'));
+          return style
+      }
+    });
+
+    this.olmap = new Map({
+      target: 'map-container',
+      layers:[
+        countryLayer,
       ],
-      controls: [],
-      view: new OlView({
-        center: this.state.center,
-        zoom: this.state.zoom
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+        projection: 'EPSG:4326'
       })
     });
+    console.log(countryLayer);
   }
 
   updateMap() {
