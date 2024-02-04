b2d3e626e6c55d6d9d6781c5c12b9d559f1764d0
code-11 <brendan.ritter@yahoo.com>
2024-02-03 22:47:20 -0500

    Added region clicking

--- stormCellGodot/MapLoader.gd
+++ stormCellGodot/MapLoader.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+var NN_CLICK_COUNT=5
+
+#func get_clicked_region(point):
+	#
+	#print(Geometry2D.is_point_in_polygon(point,poly))
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			var click_position: Vector2 = event.position
+			#var ff_poly=get_node("regions/41/poly-0").polygon
+			#print(Geometry2D.is_point_in_polygon(click_position,ff_poly))
+			var clicked_region=$regions.get_clicked_region(click_position)
+			$regions.color_border(clicked_region,"#FF0000")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +24,7 @@ func _ready():
 	)
 	$regions.create_regions()
 	$regions.color_regions(region_color_dict)
+	#print(get_node("regions/41/poly-0"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- stormCellGodot/RegionGeoLoader.gd
+++ stormCellGodot/RegionGeoLoader.gd
@@ -1,6 +1,74 @@
 extends Node2D
 
 var SCALE=1
+var LONGEST_EXTENT=null
+
+func get_clicked_region(clicked_point):
+	for region in get_children():
+		for poly in get_polys(region):
+			if Geometry2D.is_point_in_polygon(clicked_point,poly.polygon):
+				return region
+	return null
+
+func point_in_bbox(point: Vector2, bb) -> bool:
+	var maxx=bb[0]
+	var maxy=bb[1]
+	var minx=bb[2]
+	var miny=bb[3]
+	return minx <= point.x <= maxx and miny <= point.y <= maxy
+
+func long_extent(region):
+	var bb=get_region_bb(region)
+	var maxx=bb[0]
+	var maxy=bb[1]
+	var minx=bb[2]
+	var miny=bb[3]
+	var xdif=maxx-minx
+	var ydif=maxy-miny
+	if xdif > ydif:
+		return xdif
+	else:
+		return ydif
+
+func get_region_bb(region):
+	var maxx=null
+	var maxy=null
+	var minx=null
+	var miny=null
+	for poly in get_polys(region):
+		for vec in poly.polygon:
+			var x=vec[0]
+			var y=vec[1]
+			if maxx == null or maxx<x:
+				maxx=x
+			if maxy == null or maxy<y:
+				maxy=y
+			if minx == null or minx>x:
+				minx=x
+			if miny == null or miny>y:
+				miny=y
+	return [maxx,maxy,minx,miny]
+	
+func get_bb_center(bb):
+	var maxx=bb[0]
+	var maxy=bb[1]
+	var minx=bb[2]
+	var miny=bb[3]
+	return [(maxx+minx)/2,(maxy+miny)/2]	
+	
+func get_polys(region):
+	var to_return=[]
+	for child in region.get_children():
+		if child is Polygon2D:
+			to_return.append(child)
+	return to_return
+	
+func get_borders(region):
+	var to_return=[]
+	for child in region.get_children():
+		if child is Line2D:
+			to_return.append(child)
+	return to_return
 
 func array_to_packed_vec2(array):
 	var to_return=PackedVector2Array()
@@ -18,17 +86,21 @@ func create_region_geometry(region_id, polygon_list):
 	var region_geometry=Node2D.new()
 	region_geometry.name=region_id
 	
+	var i=0
 	for polygon in polygon_list:
 		var single_poly=Polygon2D.new()
 		var poly_points=array_to_packed_vec2(polygon)
 		single_poly.polygon=poly_points
+		single_poly.name="poly-%s" % i
 		region_geometry.add_child(single_poly)
 		
 		var border = Line2D.new()
 		border.points=poly_points
-		border.width=3
-		border.default_color=Color(0,0,0)
+		border.width=2
+		border.default_color=Color.html("#333333")
+		border.name="border-%s" % i
 		region_geometry.add_child(border)
+		i+=1
 	return region_geometry
 	
 func read_regions():
@@ -42,9 +114,15 @@ func read_regions():
 
 func create_regions():
 	var regions=read_regions()
-	print(regions.size())
+	print("Num Regions: %s" % regions.size())
+	var greatest_extent=0
 	for region in regions:
+		var long_extent=long_extent(region)
+		if greatest_extent < long_extent:
+			greatest_extent = long_extent
 		add_child(region)
+	LONGEST_EXTENT=greatest_extent
+	print(LONGEST_EXTENT)
 
 func color_region(region,color_hex):
 	var polys=region.get_children()
@@ -53,10 +131,14 @@ func color_region(region,color_hex):
 			poly.color=Color.html(color_hex)
 
 func color_regions(region_color_dict):
-	print(region_color_dict)
 	for region in get_children():
 		var region_color_hex=region_color_dict.get(region.name,"#D5CEAB")
 		color_region(region,region_color_hex)
+
+func color_border(region,color_hex):
+	var borders=get_borders(region)
+	for border in borders:
+		border.default_color=Color.html(color_hex)
 
 func _ready():
 	pass
